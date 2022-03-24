c1904db49627a72f16d614254f08615002c69e97
Kayla Thorne <kthornetdcp@gmail.com>
2022-03-24 11:35:48 -0400

    added testapp class

--- src/main/java/Music.java
+++ src/main/java/Music.java
@@ -1,15 +1,18 @@
-public class Music {
+public class Music extends ServiceCommunicator{
     private String music;
 
     public Music(String music) {
         this.music = music;
     }
 
+    public void getMusic() {
+        setURL("https://itunes.apple.com/search?term=" + this.music + "&limit=1");
+        connect();
+        System.out.println(get());
+    }
+
     public static void main(String[] args) {
-        ServiceCommunicator sc = new ServiceCommunicator();
-        Music music = new Music("outlander");
-        sc.setURL("https://itunes.apple.com/search?term=" + music.music + "&limit=1");
-        sc.connect();
-        System.out.println(sc.get());
+        Music music = new Music("He's-Been-Good");
+        music.getMusic();
     }
 }
